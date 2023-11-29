11f942c3fcc0f4073b06390d5b82832dc2c58f17
mythiliannamalai <100024249+mythiliannamalai@users.noreply.github.com>
2023-11-29 11:00:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@
         {
             Console.WriteLine("\n 2) Enter the year");
             int year = int.Parse(Console.ReadLine());
-            if (((year % 4 == 0) && (year % 100 == 0)) || (year % 400 == 0))
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
             {
                 Console.WriteLine("{0} is a leap year", year);
             }
